1e7a6ac44a7fe408883d8e0020f5346bc35f91e2
German Arcila <germanarcila@gmail.com>
2018-01-08 21:33:49 -0500

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Header = ({title, mantra}) => {
   return (
   <div className='header' >
-    <h1 className='title' >{title}</h1>
+    <div className='title' >
+      <img src='../faces-white.png' alt={title}/>
+      <h1>{title}</h1>
+    </div>
     <h2 className='mantra' >{mantra}</h2>
   </div>
   )
